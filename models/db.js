5c857ab50de00b4b178667116af69c53e27bfdc7
Edson de Maria <cristiandemaria34@gmail.com>
2021-10-31 22:05:14 -0300

    modificando o .gitignore

--- models/db.js
+++ models/db.js
@@ -18,7 +18,7 @@ else{
     const Sequelize = require('sequelize');
 
     //CONEXÃO COM BANCO LOCAL
-    const sequelize = new Sequelize('bd_sgp', 'root', 'senha', {
+    const sequelize = new Sequelize('bd_sgp', 'root', 'C8r7i6s5t4i3a2n1', {
         host: 'localhost',
         port: '3306',
         dialect: 'mysql'
