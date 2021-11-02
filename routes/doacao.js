8675846b009878b6982a0394a92b2c086057a613
Edson de Maria <cristiandemaria34@gmail.com>
2021-11-01 21:21:41 -0300

    criando a página de encaminhar doações

--- routes/doacao.js
+++ routes/doacao.js
@@ -1,9 +1,37 @@
 const express = require('express');
 const router = express.Router();
 
-//ROTA DE DOACAO
-router.get('/doacao', (req, res) =>{
-    res.render("admin/doacoes/doacao");
+//CARREGANDO OS MODELS
+const Doacao = require('../models/Doacao');
+const Encaminhamento = require('../models/Encaminhamento');
+
+//ROTA DA PÁGINA DOE
+router.get('/doe', (req, res) =>{
+    res.render("admin/doacoes/doe");
+});
+
+//ROTA DA PÁGINA DOAÇÕES
+router.get('/doacoes', (req, res)=>{
+    Doacao.findAll().then((doacoes)=>{
+        doacoes = doacoes.map((doacao)=>{
+            return doacao.toJSON();
+        });
+        res.render("admin/doacoes/doacoes", {
+        doacao: doacoes
+        });
+    });
+});
+
+//ROTA DA PÁGINA DE ENCAMINHAMENTO DE DOAÇÕES
+router.get('/encaminhamento', (req, res)=>{
+    Encaminhamento.findAll().then((encaminhamentos)=>{
+        encaminhamentos = encaminhamentos.map((encaminhamento)=>{
+            return encaminhamento.toJSON();
+        });
+        res.render("admin/doacoes/encaminhamento", {
+            encaminhamento: encaminhamentos
+        });
+    });
 });
 
 module.exports = router;
