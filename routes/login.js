af1b6e64a013c0348e5b82cc1f8a8a06dda0ea1d
Edson de Maria <cristiandemaria34@gmail.com>
2021-11-10 17:51:04 -0300

    Últimas atualizações

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
+//CARREGANDO OS MODELSD
+const Administrador = require('../models/Administrador')
+
 //ROTA LOGIN 
 router.get('/login',(req, res) =>{
     res.render('admin/login/login');
+});
+
+//ROTA DE VALIDAÇÃO DO LOGIN
+router.post('/loginauth', (req, res)=>{
+    var usuario = req.body.usuario;
+    var senha = req.body.senha;
+
+
+    Administrador.findOne({
+        where: {
+            tb_administrador_usuario: usuario,
+            tb_administrador_senha: senha
+        }
+    }).then((administrador)=>{
+        if(administrador){
+            res.render("admin/home_adm/home_adm");
+        }
+        else{
+            res.send("Login não efetuado");
+        }
+    }).catch((erro=>{
+        res.send("Ocorreu um erro: "+ erro);
+    }))
 });
 
 module.exports = router;
