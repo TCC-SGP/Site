b8111bc4e82e41cb6953017b6b98f736dfd42338
Paulo Ricardo <paulorbastos0@gmail.com>
2021-11-13 12:26:54 -0300

    Home - Apadrinhamento e Adoção
    Arrumei o design e  fiz 75% do front-end

--- routes/admin.js
+++ routes/admin.js
@@ -6,14 +6,35 @@ const Postagem = require('../models/Postagem');
 
 //ROTAS CENTRAIS
 router.get('/', (req, res) => {
-    Postagem.findAll().then((postagens)=>{
-        postagens = postagens.map((postagem)=>{
-            return postagem.toJSON();
-        });
-        res.render("home", {
-            postagem: postagens
-        });
+    Postagem.findAll({
+        where: { tb_tipopostagem_id: 2 },
+        order: [["createdAt", "DESC"]],
+        limit: 3
+    }).then((postagens_apad) => {
+        Postagem.findAll({
+            where: { tb_tipopostagem_id: 1 },
+            order: [["createdAt", "DESC"]],
+            limit: 3
+        }).then((postagens_publi) => {
+            Postagem.findAll({
+                where: { tb_tipopostagem_id: 3 },
+                order: [["createdAt", "DESC"]],
+                limit: 3
+            }).then((postagens_ado) => {
+                var nadpadrinhamento = JSON.parse(JSON.stringify(postagens_apad));
+                var npublicidade = JSON.parse(JSON.stringify(postagens_publi));
+                var nadocao = JSON.parse(JSON.stringify(postagens_ado));
+                res.render("home", {
+                    apadrinhamento: nadpadrinhamento,
+                    adocao: nadocao,
+                    publicidade: npublicidade
+
+                });
+            })
+        })
     });
 });
-//Carlos
-module.exports = router;
+
+
+
+module.exports = router
