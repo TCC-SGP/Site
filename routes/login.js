c38dd04c74d4382a41f5166ef4bd833d54946699
Edson de Maria <cristiandemaria34@gmail.com>
2021-11-19 20:08:45 -0300

    Tratando possiveis erros na tela de login

--- routes/login.js
+++ routes/login.js
@@ -7,79 +7,90 @@ const Administrador = require('../models/Administrador');
 const Doador = require('../models/Doador');
 
 //ROTA LOGIN 
-router.get('/login',(req, res) =>{
+router.get('/login', (req, res) => {
     res.render('admin/login/login');
 });
 
 //ROTA DE VALIDAÇÃO DO LOGIN
-router.post('/loginauth',(req, res)=>{
+router.post('/loginauth', (req, res) => {
     var usuario = req.body.usuario;
     var senha = req.body.senha;
     var identificador = req.body.identificador;
     console.log(identificador);
 
-    if(identificador == 1){
+    if (identificador == 1) {
         Administrador.findOne({
             where: {
                 tb_administrador_usuario: usuario,
-                tb_administrador_senha: senha   
+                tb_administrador_senha: senha
             }
-        }).then((administrador)=>{
-            var nadministrador  = JSON.parse(JSON.stringify(administrador));
-            let idadm = nadministrador.tb_administrador_id;
-            if(administrador){
-                const token = jwt.sign({ user_id: idadm},
-                    ""+process.env.TOKEN_KEY,
-                    {
-                        expiresIn: "2h",
-                    }
-                );
-    
-                Administrador.token = token;    
-                res.cookie("token", token, {maxAge: 24 * 60 * 60 * 1000, httpOnly: true});
-                console.log("JWT token está nos cookies");
-                res.redirect("/home_adm");
-                console.log(token);
+        }).then((administrador) => {
+            try {
+                var nadministrador = JSON.parse(JSON.stringify(administrador));
+                let idadm = nadministrador.tb_administrador_id;
+
+                if (administrador) {
+                    const token = jwt.sign({ user_id: idadm },
+                        "" + process.env.TOKEN_KEY,
+                        {
+                            expiresIn: "2h",
+                        }
+                    );
+
+                    Administrador.token = token;
+                    res.cookie("token", token, { maxAge: 24 * 60 * 60 * 1000, httpOnly: true });
+                    console.log("JWT token está nos cookies");
+                    res.redirect("/home_adm");
+                    console.log(token);
+                }
+                else {
+                    res.send("<script>alert('Login não efetuado')</script>");
+                }
             }
-            else{
-                res.send("<script>alert('Login não efetuado')</script>");
+            catch(err){
+                res.render("admin/login/login", { errorMessage: "Administrador não encontrado"})
             }
-        }).catch((erro=>{
-            console.log(erro);
-            res.render("admin/login/login", {errorMessage: "Usuário ou senha inválidos"});
-        }))
+        }).catch((erro => {
+                console.log(erro);
+                res.render("admin/login/login", { errorMessage: "Usuário ou senha inválidos" });
+            }))
     }
-    else if (identificador == 2){
+    else if (identificador == 2) {
         Doador.findOne({
             where: {
                 tb_doador_usuario: usuario,
                 tb_doador_senha: senha
             }
-        }).then((doador)=>{
-            var ndoador = JSON.parse(JSON.stringify(doador));
-            let iddoador = ndoador.tb_doador_id;
+        }).then((doador) => {
+            try {
+                var ndoador = JSON.parse(JSON.stringify(doador));
+                let iddoador = ndoador.tb_doador_id;
 
-            if(doador){
-                const token = jwt.sign({user_id: iddoador},
-                    ""+process.env.TOKEN_KEY,
-                    {
-                        expiresIn: "2h",
-                    }
-                );
+                if (doador) {
+                    const token = jwt.sign({ user_id: iddoador },
+                        "" + process.env.TOKEN_KEY,
+                        {
+                            expiresIn: "2h",
+                        }
+                    );
 
-                Doador.token = token;
-                res.cookie("token", token, {maxAge: 24 * 60 * 60 * 1000, httpOnly: true});
-                console.log("JWT token está nos cookies");
-                res.redirect("/home_adm");
-                console.log(token);
+                    Doador.token = token;
+                    res.cookie("token", token, { maxAge: 24 * 60 * 60 * 1000, httpOnly: true });
+                    console.log("JWT token está nos cookies");
+                    res.redirect("/home_adm");
+                    console.log(token);
+                }
+            }
+            catch (err) {
+                res.render("admin/login/login", { errorMessage: "Doador não encontrado" });
             }
         })
     }
     else {
-        res.render("admin/login/login", {errorMessage: "Por favor selecione o tipo de usuário!"});
+        res.render("admin/login/login", { errorMessage: "Por favor selecione o tipo de usuário!" });
     }
 
-    
+
 });
 
 module.exports = router;
